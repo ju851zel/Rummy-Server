b5c1b68ba0c8bf71b0e4fbcd7b84e8dd81bfbdc6
julian <julianMBP>
2019-11-16 01:25:21 +0100

    - added basic functionality - very buggy

--- app/controllers/RummyController.scala
+++ app/controllers/RummyController.scala
@@ -27,25 +27,112 @@ class RummyController @Inject()(cc: ControllerComponents) extends AbstractContro
     Ok(views.html.game(controller))
   }
 
-  def createGame(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  def state(): Action[AnyContent] = Action {
+    val string: String = controller.currentControllerState match {
+      case ControllerState.P_TURN =>
+        "P_TURN"
+      case ControllerState.KILL =>
+        "KILL"
+      case ControllerState.INSERTING_NAMES =>
+        "INSERTING_NAMES"
+      case ControllerState.NEXT_TYPE_N =>
+        "NEXT_TYPE_N"
+      case _ =>
+        "MENU"
+    }
+    Ok(string)
+  }
+
+  def json(): Action[AnyContent] = Action {
+    Ok(controller.toJson().toString())
+  }
+
+  def news(): Action[AnyContent] = Action {
+    val string = controller.currentAnswerState match {
+      case AnswerState.P_FINISHED =>
+        "You are finished."
+      case AnswerState.TABLE_NOT_CORRECT =>
+        "Table looks not correct, please move tiles to match the rules or undo until it matches"
+      case AnswerState.P_WON =>
+        s"${controller.getCurrentPlayer.name} is the winner."
+      case AnswerState.UNDO_TAKE_TILE =>
+        "The tile has been put back in the bag"
+      case AnswerState.BAG_IS_EMPTY =>
+        "No more tiles in the bag. You must lay a tile down"
+      case AnswerState.CANT_MOVE_THIS_TILE =>
+        "You can not move this tile."
+      case AnswerState.UNDO_MOVED_TILE =>
+        "You undid the move of a specific tile."
+      case AnswerState.MOVED_TILE =>
+        "You did move a tile to another."
+      case AnswerState.UNDO_LAY_DOWN_TILE =>
+        "You undid the lay down you took the tile up."
+      case AnswerState.ADDED_PLAYER =>
+        "You added a player."
+      case AnswerState.PUT_TILE_DOWN =>
+        "You put down a tile"
+      case AnswerState.PLAYER_REMOVED =>
+        "You removed the player you inserted."
+      case AnswerState.P_DOES_NOT_OWN_TILE =>
+        "You dont have this tile on the board. Please select another one"
+      case AnswerState.INSERTING_NAMES_FINISHED =>
+        "You finished inserting the names."
+      case AnswerState.STORED_FILE =>
+        "You stored the game in a file"
+      case AnswerState.NOT_ENOUGH_PLAYERS =>
+        "Not enough Players. Add some more."
+      case AnswerState.COULD_NOT_LOAD_FILE =>
+        "Could not load the file. Created a new game instead."
+      case AnswerState.LOADED_FILE =>
+        "You loaded a file"
+      case AnswerState.CREATED_DESK =>
+        "You started the game by creating a desk"
+      case AnswerState.UNDO_MOVED_TILE_NOT_DONE =>
+        "Undo the move of the tile unnecessary. Nothing did happen."
+      case AnswerState.P_FINISHED_UNDO =>
+        "Its is again your turn. "
+      case _ =>
+        "Lets start, shall we?"
+    }
+    Ok(string)
+  }
+
+  def todo(): Action[AnyContent] = Action {
+    val string: String = controller.currentControllerState match {
+      case ControllerState.P_TURN =>
+        s"${controller.getCurrentPlayer.name} it's your turn."
+      case ControllerState.KILL =>
+        "Finished"
+      case ControllerState.INSERTING_NAMES =>
+        "Insert a players name. (Min 2 players, Max 4) or finish with 'f'"
+      case ControllerState.NEXT_TYPE_N =>
+        "It is the next players turn"
+      case _ =>
+        "Click On Create Desk."
+    }
+    Ok(string)
+  }
+
+
+  def createGame(): Action[AnyContent] = Action {
     if (controller.currentControllerState == ControllerState.MENU) {
       controller.createDesk(12)
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson().toString())
   }
 
   def loadFile()(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     if (controller.currentControllerState == ControllerState.MENU) {
       controller.loadFile()
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
-  def finishNameInput(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  def finishNameInput(): Action[AnyContent] = Action {
     if (controller.currentControllerState == ControllerState.INSERTING_NAMES) {
       controller.nameInputFinished()
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
   def undo(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
@@ -53,7 +140,7 @@ class RummyController @Inject()(cc: ControllerComponents) extends AbstractContro
       || controller.currentControllerState == ControllerState.P_TURN) {
       controller.undo()
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
 
@@ -62,28 +149,28 @@ class RummyController @Inject()(cc: ControllerComponents) extends AbstractContro
       || controller.currentControllerState == ControllerState.P_TURN) {
       controller.redo()
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
-  def addPlayer(name: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  def addPlayer(name: String): Action[AnyContent] = Action {
     if (controller.currentControllerState == ControllerState.INSERTING_NAMES) {
       controller.addPlayerAndInit(name, 12)
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson().toString())
   }
 
   def switchToNextPlayer(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     if (controller.currentControllerState == ControllerState.NEXT_TYPE_N) {
       controller.switchToNextPlayer()
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
   def storeFile(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     if (controller.currentControllerState == ControllerState.P_TURN) {
       controller.storeFile()
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
   def rules(): Action[AnyContent] = Action {
@@ -95,7 +182,7 @@ class RummyController @Inject()(cc: ControllerComponents) extends AbstractContro
     if (controller.currentControllerState == ControllerState.P_TURN) {
       controller.layDownTile(Tile.stringToTile(tile))
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
   def moveTile(tile: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
@@ -110,14 +197,14 @@ class RummyController @Inject()(cc: ControllerComponents) extends AbstractContro
         tileToMove = Some(Tile.stringToTile(tile))
       }
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
   def finishTurn(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     if (controller.currentControllerState == ControllerState.P_TURN) {
       controller.userFinishedPlay()
     }
-    Ok(views.html.game(controller))
+    Ok(controller.toJson())
   }
 
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,29 +1,249 @@
-document.getElementById("btn-undo").addEventListener("click", undo);
-document.getElementById("btn-confirmName").addEventListener("click", addPlayer);
-document.getElementById("btn-finishName").addEventListener("click", finishNameInput);
+let game = {
+    sets: []
+};
 
-
-function addPlayer() {
-    let name = document.getElementById("input-name").value;
-    return window.location.href = "http://localhost:9000/addPlayer/" + name;
-}
-
-function finishNameInput() {
-    return window.location.href = "http://localhost:9000/finishNameInput"
-}
 
 function undo() {
     console.log("Button undo pressed")
 }
 
-function newsDiv() {
-    console.log("Test");
-    document.getElementById("newsDiv").setAttribute("class", "shadow-lg");
+function createMenuButtons() {
+    $("#interaction").empty();
+    $("#interaction").append(
+        '<button class="btn btn-primary" id="btnCreate">Create Desk</button>' +
+        '<button class="btn btn-primary" id="btnLoad">Load file</button>'
+    );
+    $("#btnCreate").click(() => {
+        $.ajax({
+            method: "GET",
+            url: "/game/create",
+            dataType: "json",
+            success: result => {
+                game = result;
+                console.log(result);
+                update()
+            }
+        })
+    })
+}
+
+function createInsertingNamesButtons() {
+    $("#interaction").empty();
+    $("#interaction").append(
+        "<div class=\"form-group\">" +
+        "    <label for=\"input-name\">Name</label>" +
+        "    <input type=\"text\" class=\"form-control\" id=\"input-name\" placeholder=\"John\">" +
+        "</div>" +
+        "<button class=\"btn btn-primary\" id=\"btnConfirmName\">Confirm Name</button>" +
+        "<button class=\"btn btn-primary\" id=\"btnFinishName\">FinishNameInput</button>"
+    );
+    $("#btnConfirmName").click(() => {
+        let name = $("#input-name").val();
+        if (name === "") {
+            updateNews("Please insert a name");
+            return
+        }
+        $.ajax({
+            method: "GET",
+            url: "/game/name/add/" + name,
+            dataType: "json",
+            success: result => {
+                game = result;
+                console.log(result);
+                update()
+            }
+        })
+    });
+    $("#btnFinishName").click(() => {
+        $.ajax({
+            method: "GET",
+            url: "/game/name/finish",
+            dataType: "json",
+            success: result => {
+                game = result;
+                console.log(result);
+                update()
+            }
+        })
+    })
+}
+
+function createTurnButtons() {
+    $("#interaction").empty();
+    $("#interaction").append(
+        "<button class=\"btn btn-primary\" id=\"btnFinishTurn\">Finished Turn</button>"
+    );
+    $("#btnFinishTurn").click(() => {
+        $.ajax({
+            method: "GET",
+            url: "/game/player/finish",
+            dataType: "json",
+            success: result => {
+                game = result;
+                $("#board").empty();
+                update()
+            }
+        })
+    })
+}
+
+function createNextButtons() {
+    $("#interaction").empty();
+    $("#interaction").append(
+        "<button class=\"btn btn-primary\" id=\"btnNextPlayer\">Next Player</button>"
+    );
+    $("#btnNextPlayer").click(() => {
+        $.ajax({
+            method: "GET",
+            url: "/game/player/next",
+            dataType: "json",
+            success: result => {
+                game = result;
+                update()
+            }
+        })
+    });
+
+}
+
+function initButtons() {
+    $.ajax({
+        method: "GET",
+        url: "/state",
+        dataType: "text",
+
+        success: result => {
+            switch (result) {
+                case "MENU":
+                    createMenuButtons();
+                    break;
+                case "INSERTING_NAMES":
+                    createInsertingNamesButtons();
+                    break;
+                case "P_TURN":
+                    createTurnButtons();
+                    break;
+                case "NEXT_TYPE_N":
+                    createNextButtons();
+                    break;
+            }
+        }
+    });
+}
+
+function init() {
+    $.ajax({
+        method: "GET",
+        url: "/json",
+        dataType: "json",
+
+        success: result => {
+            game = result;
+            console.log(result);
+        }
+    });
+}
+
+function initDesk() {
+    $("#desk").empty();
+    let row = 0;
+    for (let sorted of game.desk.sets) {
+        $("#desk").append("<div class=\"row my-4\" id=\"deskrow" + row + "\"></div>");
+        for (let tile of sorted.struct) {
+            $("#deskrow" + row).append(`
+                <div class="mx-1 align-content-center text-center ${determineColorOfTile(tile)}">
+                   <h3 class="text-white">${tile.value}</h3>
+                   <button id="${tile.value + tile.color[0] + tile.ident}" class="btn btn-link">Move</button>
+                </div>
+            `)
+        }
+        row += 1;
+    }
+    $("#desk").one("click", (event) => $.ajax({
+        method: "GET",
+        url: "/game/player/move/" + event.target.id,
+        dataType: "json",
+        success: result => {
+            game = result;
+            update()
+        }
+    }));
+}
+
+function initBoard() {
+    $("#board").empty();
+    for (let tile of game.desk.players.find(player => player.state.toString() === "TURN").board) {
+        $("#board").append(`
+            <div class="mx-1 align-content-center text-center ${determineColorOfTile(tile)}">
+                <h3 class="text-white">${tile.value}</h3>
+               <button id="${tile.value + tile.color[0] + tile.ident}" class="btn btn-link">Move</button>
+            </div>
+        `)
+    }
+    $("#board").one("click", (event) => $.ajax({
+        method: "GET",
+        url: "/game/player/laydown/" + event.target.id,
+        dataType: "json",
+        success: result => {
+            game = result;
+            update()
+        }
+    }));
+
+}
+
+function determineColorOfTile(tile) {
+    switch (tile.color) {
+        case "YELLOW":
+            return "tileY";
+        case "RED":
+            return "tileR";
+        case "GREEN":
+            return "tileG";
+        case "BLUE":
+            return "tileB";
+    }
+}
+
+function update() {
+    initButtons();
+    updateTodo();
+    updateNews();
+    initDesk();
+    initBoard();
+}
+
+function updateTodo() {
+    $.ajax({
+        method: "GET",
+        url: "/todo",
+        dataType: "text",
+
+        success: result => {
+            $("#todo").text(result);
+        }
+    });
+}
+
+function updateNews(string = "") {
+    if (string !== "") {
+        $("#news").text(string);
+        return
+    }
+    $.ajax({
+        method: "GET",
+        url: "/news",
+        dataType: "text",
+
+        success: result => {
+            $("#news").text(result);
+        }
+    });
 }
 
 
-
-
-$(document).ready(function() {
+$(document).ready(function () {
     console.log('The DOM is ready!');
+    init();
+    update()
 });
